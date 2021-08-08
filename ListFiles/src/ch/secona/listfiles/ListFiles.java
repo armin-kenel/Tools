b2874135550ba8fd41bc364efb04d95bfd6a44a5
Armin Kenel <armin.kenel@secona.ch>
2021-08-08 12:59:06 +0200

    export date/time too, some enhancements

--- ListFiles/src/ch/secona/listfiles/ListFiles.java
+++ ListFiles/src/ch/secona/listfiles/ListFiles.java
@@ -1,6 +1,11 @@
 package ch.secona.listfiles;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,9 +13,11 @@ import java.util.List;
 import javax.swing.filechooser.FileSystemView;
 
 /**
- * Lists each file on a single line
+ * Lists each file (and files in its sub directories) on a single line
  */
 public class ListFiles {
+	private static final String DASH_POINT = ";";
+	private static final char DOUBLE_POINT = ':';
 	private final String toList;
 	private final String volume;
 
@@ -27,18 +34,22 @@ public class ListFiles {
 		volume = getVolumeLabel();
 	}
 
+	/**
+	 * @return the volume label for the volume used in 'toList'
+	 */
 	private String getVolumeLabel() {
 		final List<File> files = Arrays.asList(File.listRoots());
+		final int pos = toList.indexOf(DOUBLE_POINT);
 		String drive = null;
-		final int pos = toList.indexOf(':');
+
 		if (pos > 0) {
 			drive = toList.substring(0, pos + 1).toUpperCase();
 		}
-		for (final File f : files) {
-			final String s1 = FileSystemView.getFileSystemView().getSystemDisplayName(f);
+		for (final File file : files) {
+			final String systemDisplayName = FileSystemView.getFileSystemView().getSystemDisplayName(file);
 
-			if (drive != null && s1.contains(drive)) {
-				return s1;
+			if (drive != null && systemDisplayName.contains(drive)) {
+				return systemDisplayName;
 			}
 		}
 		return "";
@@ -62,13 +73,38 @@ public class ListFiles {
 			final long length = file.length();
 			final int l = absolutePath.length() - name.length() - 1;
 			final String directory = absolutePath.substring(0, l);
+			final String lastModified = getLastModified(absolutePath);
 
-			System.out.println(name + ";" + directory + ";" + length + ";" + volume);
+			System.out.println(name + DASH_POINT + directory + DASH_POINT + //
+			length + DASH_POINT + lastModified + DASH_POINT + volume);
 		});
 		System.out.println("");
 		line();
 		System.out.println("Done.");
 		line();
+	}
+
+	private String getLastModified(final String absolutePath) {
+		final Path path = Paths.get(absolutePath);
+		String lastModified = "";
+
+		try {
+			final BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+
+			lastModified = "" + attr.lastModifiedTime();
+
+			final int indexOfPoint = lastModified.indexOf('.');
+
+			if (indexOfPoint > 0) {
+				lastModified = lastModified.substring(0, indexOfPoint);
+			}
+			lastModified = lastModified.replace('T', ' ');
+			lastModified = lastModified.replace("Z", "");
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lastModified;
 	}
 
 	private static void line() {
@@ -77,7 +113,8 @@ public class ListFiles {
 
 	/**
 	 * reads all files in a given directory and its sub directories and adds the
-	 * file name to the passed list.
+	 * file names to the passed list.<br/>
+	 * hidden files and directories will be ignored.
 	 *
 	 * @param folder   folder to read
 	 * @param fileList list where the read file names are added
